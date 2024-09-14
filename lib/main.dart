ecc986558114abce5a7afc01b7bbe8a48bb2c43b
Mitsutan <petitcom_hondou@icloud.com>
2024-09-14 16:52:45 +0900

    bluetooth発信、受信
    一応できたっぽい

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_beacon/flutter_beacon.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,6 +37,47 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  final MsdFilter _msdFilterData = MsdFilter(76, data: [
+    0x02,
+    0x15,
+    0x97,
+    0xb7,
+    0x57,
+    0x1b,
+    0x57,
+    0x18,
+    0xbc,
+    0x11,
+    0xa7,
+    0xd3,
+    0x86,
+    0x02,
+    0x4c,
+    0xda,
+    0x3b,
+    0x5c
+  ], mask: [
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1
+  ]);
+
   @override
   void initState() {
     _init();
@@ -53,13 +95,27 @@ class _MyHomePageState extends State<MyHomePage> {
   void _startBeacon() async {
     try {
       await flutterBeacon.startBroadcast(BeaconBroadcast(
-          proximityUUID: "124586a6-8b4f-213b-0a00-3098e304ed50",
+          proximityUUID: "97b7571b-5718-bc11-a7d3-86024cda3b5c",
           major: 0,
           minor: 5,
           identifier: "dev.mitsutan.spajam2024_kumamoto"));
     } catch (e) {
       log('Start broadcast error', name: 'beacon', error: e);
     }
+
+    try {
+      await FlutterBluePlus.startScan(
+          withMsd: [_msdFilterData], androidUsesFineLocation: true);
+    } catch (e) {
+      log('Start scan Err', name: 'beacon', error: e);
+    }
+
+    FlutterBluePlus.scanResults.listen((results) {
+      // updateScanResults(results);
+      log('Scan results: $results', name: 'FlutterBluePlus');
+    }, onError: (e) {
+      log('Scan error', name: 'FlutterBluePlus', error: e);
+    });
   }
 
   @override
